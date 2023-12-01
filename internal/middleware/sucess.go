1880ed43f422eeba06a389279034dec0f68b17c7
itsfeas <amf.fiaz@gmail.com>
2023-12-01 01:09:48 -0700

    success responder complete

--- internal/middleware/sucess.go
+++ internal/middleware/sucess.go
@@ -5,19 +5,47 @@ import (
 	"net/http"
 )
 
-func SuccessResponse(h http.Handler) http.Handler {
+// SuccessResponseMiddleware is a middleware that sends a success JSON response for successful requests
+func SuccessResponseMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
+		// Create a custom ResponseWriter to intercept the status code
+		customResponseWriter := &CustomResponseWriter{ResponseWriter: w}
 
-		message := map[string]string{"status": "ok"}
-		jsonResponse, err := json.Marshal(message)
+		// Call the next handler in the chain
+		next.ServeHTTP(customResponseWriter, r)
 
-		if err != nil {
-			return err
+		// Check if the status code is within the success range (200-299)
+		if customResponseWriter.statusCode >= http.StatusOK && customResponseWriter.statusCode < http.StatusMultipleChoices {
+			// Create a generic success response
+			successResponse := map[string]interface{}{
+				"success": true,
+				"message": "Request was successful.",
+				// You can include additional data if needed
+			}
+
+			// Convert the success response to JSON
+			jsonResponse, err := json.Marshal(successResponse)
+			if err != nil {
+				http.Error(w, "Error encoding JSON", http.StatusInternalServerError)
+				return
+			}
+
+			// Set the content type to JSON and write the response
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusOK)
+			w.Write(jsonResponse)
 		}
-
-		w.WriteHeader(http.StatusOK)
-		w.Write(jsonResponse)
-		return nil
 	})
+}
+
+// CustomResponseWriter is a custom ResponseWriter that intercepts the status code
+type CustomResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+// WriteHeader intercepts the status code and stores it
+func (w *CustomResponseWriter) WriteHeader(code int) {
+	w.statusCode = code
+	w.ResponseWriter.WriteHeader(code)
 }
