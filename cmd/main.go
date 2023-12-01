0dbd677f2cf15973dcfb9795611baef90123a1be
itsfeas <amf.fiaz@gmail.com>
2023-12-01 02:58:59 -0700

    attempt to integrate middleware

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,25 @@ package cmd
 import (
 	"fmt"
 	"net/http"
+	"livin-on-a-platter-api/internal/middleware"
 )
+
+// YourHandler is the main handler for your route
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	// Your logic here
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hello, Middleware!"))
+}
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Hi there, I'm livin-on-a-platter-api!</h1>")
 	})
+
+	handler := http.HandleFunc("/api/", YourHandler)
+	http.HandleFunc(middleware.MiddlewareManager(
+		handler, middleware.SuccessResponseMiddleware, middleware.ErrorHandler)
+	)
 
 	fmt.Println("Server is listening on port 8080...")
 	http.ListenAndServe(":8080", nil)
