0400fda8b80a3e4efb1c823937ee3ef38cefd1fe
itsfeas <amf.fiaz@gmail.com>
2023-12-02 16:57:57 -0700

    add test endpt

--- cmd/main.go
+++ cmd/main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
-	"livin-on-a-platter-api/pkg/middleware"
+	"livin-on-a-platter-api/internal/db/firebase"
+	"livin-on-a-platter-api/internal/middleware"
+	"livin-on-a-platter-api/internal/model"
+	"livin-on-a-platter-api/internal/repository"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 // YourHandler is the main handler for your route
 func YourHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
+	repo := repository.NewImageRepository()
+	repo.Create(&model.ImageUpload{
+		ID:        uuid.New(),
+		FileName:  "file",
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	})
 	fmt.Println("message received")
 }
 
 func main() {
+	firebase.FirebaseDB().Connect()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Hi there, I'm livin-on-a-platter-api!</h1>")
 	})
