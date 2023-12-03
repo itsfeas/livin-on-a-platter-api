d965bfc2b0cb8776b671a99737f692b91569bea4
itsfeas <amf.fiaz@gmail.com>
2023-12-02 22:20:01 -0700

    new cors manager middleware

---
+++ internal/middleware/cors.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+func CorsManagerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		// Handle preflight requests
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Call the next handler in the chain
+		next.ServeHTTP(w, r)
+	})
+}
