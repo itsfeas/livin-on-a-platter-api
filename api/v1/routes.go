d12585adfaf76fb87572e118cd8ba20a42ab4762
itsfeas <amf.fiaz@gmail.com>
2023-12-04 23:11:40 -0700

    update routes

--- api/v1/routes.go
+++ api/v1/routes.go
@@ -9,6 +9,6 @@ import (
 func Routes() http.Handler {
 	mux := http.NewServeMux()
 	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", routes.UploadRoutes()))
-	wrappedRoutes := middleware.MiddlewareManager(mux, middleware.CorsManagerMiddleware, middleware.ErrorHandler)
+	wrappedRoutes := middleware.MiddlewareManager(mux, middleware.CorsManagerMiddleware, middleware.ErrorHandlerMiddleware)
 	return wrappedRoutes
 }
