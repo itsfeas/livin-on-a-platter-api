055fa5ba9e7fb10ce9cf71b34c8e8b569bdc311b
itsfeas <amf.fiaz@gmail.com>
2024-01-08 12:20:55 -0700

    img2img client api call signature changed

--- internal/client/ai/img2img/img2img_client.go
+++ internal/client/ai/img2img/img2img_client.go
@@ -1,11 +1,13 @@
 package img2img
 
 import (
+	"encoding/json"
+	msg "livin-on-a-platter-api/internal/model/msg/types"
 	"net/http"
 	"os"
 )
 
-func GenerateImg(queuedImgId string) error {
+func GenerateImg(queuedImgId string) (*msg.BaseMsg, error) {
 	apiUrl := os.Getenv("AI_API_URL")
 	//body := &msg2.DataMsg{
 	//	BaseMsg: msg2.DefaultBaseMsg(),
@@ -14,8 +16,11 @@ func GenerateImg(queuedImgId string) error {
 	//		uploadId: uploadId,
 	//	},
 	//}
-	if _, err := http.Get(apiUrl + "generate/" + queuedImgId); err != nil {
-		return err
+	response, err := http.Get(apiUrl + "generate/" + queuedImgId)
+	if err != nil {
+		return nil, err
 	}
-	return nil
+	msg := &msg.BaseMsg{}
+	json.NewDecoder(response.Body).Decode(&msg)
+	return msg, nil
 }
