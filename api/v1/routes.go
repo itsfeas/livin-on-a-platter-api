737a6c18f4e5a2a8ded62db3a485694b22208a81
itsfeas <amf.fiaz@gmail.com>
2024-01-08 21:20:44 -0700

    routes incorporated

--- api/v1/routes.go
+++ api/v1/routes.go
@@ -1,14 +1,16 @@
 package api
 
 import (
-	routes "livin-on-a-platter-api/api/v1/routes/uploads"
+	"livin-on-a-platter-api/api/v1/routes/upload_routes"
+	"livin-on-a-platter-api/api/v1/routes/view_routes"
 	"livin-on-a-platter-api/internal/middleware"
 	"net/http"
 )
 
 func Routes() http.Handler {
 	mux := http.NewServeMux()
-	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", routes.UploadRoutes()))
+	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", upload_routes.UploadRoutes()))
+	mux.Handle("/api/v1/", http.StripPrefix("/api/v1", view_routes.ViewRoutes()))
 	wrappedRoutes := middleware.MiddlewareManager(mux, middleware.CorsManagerMiddleware, middleware.ErrorHandlerMiddleware)
 	return wrappedRoutes
 }
